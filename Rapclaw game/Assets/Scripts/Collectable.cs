44aaac18eacff3e3f424ff9c5bf1d630fac901db
Reverbraptor42 <55810546+Reverbraptor42@users.noreply.github.com>
2019-09-25 15:42:00 -0500

    Initial project commit

---
+++ Rapclaw game/Assets/Scripts/Collectable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		HandleCollected();
+	}
+
+	void HandleCollected()
+	{
+		print("I've been collected");
+		Destroy(gameObject);
+	}
+}
