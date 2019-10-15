10afd80b62ec4159851c1470b87ceab441019c63
Reverbraptor42 <55810546+Reverbraptor42@users.noreply.github.com>
2019-10-15 15:38:57 -0500

    soundoncompletion

---
+++ Rapclaw game/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    public AudioSource audioSource;
+
+    public AudioSource completeSource;
+
+    public AudioClip clawCollectSound;
+
+    public AudioClip allClawsCollected;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void PlayClawCollect()
+    {
+       audioSource.clip = clawCollectSound;
+        audioSource.Play();
+    }
+
+    public void PlayClawsCollected()
+    {
+        completeSource.clip = allClawsCollected;
+        completeSource.Play();
+    }
+}    
---
+++ Rapclaw game/Assets/Scripts/YouWinMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class YouWinMenu : MonoBehaviour
+{
+    public static YouWinMenu Instance;
+    public GameObject container;
+    public Button nextLevelButton;
+    public Button hideButton;
+
+    private void OnEnable()
+    {
+        nextLevelButton.onClick.AddListener(HandleNextLevelPressed);
+    }
+
+    private void OnDisable()
+    {
+        nextLevelButton.onClick.RemoveListener(HandleNextLevelPressed);
+    }
+
+    private void HandleNextLevelPressed()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Show()
+    {
+        container.SetActive(true);
+        SoundManager.Instance.PlayClawsCollected();
+    }
+    
+}
